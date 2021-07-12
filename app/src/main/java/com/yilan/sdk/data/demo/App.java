3b58508950c27b5ec3be8118eba56c296712aa7b
liurongzhi <360465033@qq.com>
2021-07-12 10:26:10 +0800

    【功能】提交demo

--- app/src/main/java/com/yilan/sdk/data/demo/App.java
+++ app/src/main/java/com/yilan/sdk/data/demo/App.java
@@ -2,18 +2,28 @@ package com.yilan.sdk.data.demo;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+import com.yilan.sdk.ylad.YLAdInit;
+
 /**
  * Author And Date: liurongzhi on 2021/6/19.
  * Description: com.yilan.sdk.data.demo
  */
 public class App extends Application {
     public static App app;
-    public static final String YL_KEY = "ylel2vek386q";
-    public static final String YL_TOKEN = "talb5el4cbw3e8ad3jofbknkexi1z8r4";
+    public static final String YL_KEY = "";
+    public static final String YL_TOKEN = "";
 
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
+        LeakCanary.install(this);
+        YLAdInit.getInstance()
+                .setApplication(this)
+                .setAccessKey(YL_KEY)
+                .setAccessToken(YL_TOKEN)
+                .logEnable(true)
+                .build();
     }
 }
